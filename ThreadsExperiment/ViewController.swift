00a014cbd79a1aac65233ead21e93043ff150ee6
Simon Gladman <sgladman@Simon-Gladmans-MacBook-Pro.local>
2014-08-02 07:26:34 +0100

    wip

--- ThreadsExperiment/ViewController.swift
+++ ThreadsExperiment/ViewController.swift
@@ -14,15 +14,15 @@ class ViewController: UIViewController
     @IBOutlet var imageView: UIImageView!
     
     let arraySide = 100;
-    var grayScottData = Array<Array<(CGFloat,CGFloat,CGFloat)>>()
+    var grayScottData = Array<Array<(CGFloat,CGFloat)>>()
 
     override func viewDidLoad()
     {
-        super.viewDidLoad()
+        super.viewDidLoad();
      
         for column in 0..<arraySide
         {
-            grayScottData.append(Array(count:arraySide, repeatedValue:(CGFloat(),CGFloat(),CGFloat())))
+            grayScottData.append(Array(count:arraySide, repeatedValue:(CGFloat(),CGFloat())))
         }
      
         UIGraphicsBeginImageContextWithOptions(CGSize(width: arraySide, height: arraySide), true, 1);
@@ -32,39 +32,23 @@ class ViewController: UIViewController
         {
             for j in 0 ..< arraySide
             {
-                var color : (CGFloat,CGFloat,CGFloat);
+                var color : (CGFloat,CGFloat);
                 
                 if arc4random() % 10 > 5
                 {
-                    color = (CGFloat(0.5), CGFloat(0.5), CGFloat(0.9));
+                    color = (CGFloat(0.2), CGFloat(0.9));
                 }
                 else
                 {
-                    color = (CGFloat(0.9), CGFloat(0.9), CGFloat(0.2));
+                    color = (CGFloat(0.9), CGFloat(0.2));
                 }
                 
                 grayScottData[i][j] = color;
                 
-                CGContextSetRGBFillColor (context, color.0, color.1, color.2, 1);
+                CGContextSetRGBFillColor (context, color.0, color.0, color.1, 1);
                 CGContextFillRect (context, CGRectMake (CGFloat(i), CGFloat(j), 1, 1));
             }
         }
-        
-
-        
-        /*
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: 100, height: 100), true, 1);
-        let context = UIGraphicsGetCurrentContext();
-  
-        CGContextSetRGBFillColor (context, 1, 1, 0, 1)
-        CGContextFillRect (context, CGRectMake (0, 0, 200, 200))
-        CGContextSetRGBFillColor (context, 1, 0, 0, 1)
-        CGContextFillRect (context, CGRectMake (0, 0, 100, 100))
-        CGContextSetRGBFillColor (context, 1, 1, 0, 1)
-        CGContextFillRect (context, CGRectMake (0, 0, 50, 50))
-        CGContextSetRGBFillColor (context, 0, 0, 1, 0.5);
-        CGContextFillRect (context, CGRectMake (0, 0, 50, 100))
-        */
         
         var image = UIGraphicsGetImageFromCurrentImageContext();
         
@@ -72,8 +56,26 @@ class ViewController: UIViewController
 
         imageView.image = image;
 
+     
     }
 
+    
+ 
+    /*
+    override func update(currentTime: CFTimeInterval)
+    {
+        for i in 1 ..< arraySide - 1
+        {
+            for j in 1 ..< arraySide - 1
+            {
+                let thisPixel = grayScottData[i][j];
+                let northPixel = grayScottData[i][j + 1];
+                let southPixel = grayScottData[i][j - 1];
+            }
+        }
+    }
+*/
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
